02f36fbcab13391f8597c57214b50a12d0a2dbe7
Sergey Khokhlov <sergeyakhokhlov@gmail.com>
2019-10-25 20:18:54 +0300

    Test contract method call

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,16 +1,45 @@
+const contractData = {
+    address: '0x7de3a127737827d6ffef07e0125914d0a9eab3cf',
+    abi: [
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "priceBTC",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "pure",
+            "type": "function"
+        }
+    ]
+};
+
 window.addEventListener('load', async () => {
     if (window.ethereum) {
-        window.web3 = new Web3(ethereum);
+        window.web3 = new Web3(window.ethereum);
         try {
             // Request account access if needed
-            await ethereum.enable();
-            // Acccounts now exposed
-            // web3.eth.sendTransaction({/* ... */});
+            await window.ethereum.enable();
 
+            var MyContract = new window.web3.eth.Contract(contractData.abi, contractData.address);
+            var result = MyContract.methods.priceBTC().call().then(function (result) {
+                console.log(result);
+            });
         } catch (error) {
-            console.error("User denied account access..." + error);
+            console.error('User denied account access...' + error);
+            updateBanner('User denied account access...');
         }
     } else {
         console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        updateBanner('Non-Ethereum browser detected. You should consider trying MetaMask!');
     }
 });
+
+function updateBanner(text) {
+    document.getElementById("banner").innerText = text;
+}
